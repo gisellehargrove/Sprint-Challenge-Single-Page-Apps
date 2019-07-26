b6bdb11864372ce5c5b16f1c3b5f5b182d986574
gisellehargrove <gisellehargrove@gmail.com>
2019-07-26 13:50:40 -0400

    Stretch Progress

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
+import { useInput } from './useLocalStorage';
+
 
 export default function CharacterList() {
-
+  const [query, setQuery, handleQuery] = useInput('query', 'https://rickandmortyapi.com/api/character/')
   const [characters, setCharacters] = useState({});
+
+  const onSearch = (e, name) => {
+    e.preventDefault();
+    handleQuery(query + '?name=' + name.name);
+  }
 
   useEffect(() => {
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios.get('https://rickandmortyapi.com/api/character/').then(response => {
-      console.log(response.data);
+    axios.get(query).then(response => {
       setCharacters(response.data);
     });
-  }, [])
+  }, [query])
 
   if(!characters.results) return <h1>Loading...</h1>
 
-  return (<section className='character-list grid-view'>
+  return (
+
+    <section className='character-list grid-view'>
+      <div className="search-container">
+        <SearchForm onSearch={onSearch} />
+      </div>
 
       {characters.results.map((character) =>
         <CharacterCard

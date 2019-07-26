41df783de1298afaa48f9ed21dfed28d7484b45d
gisellehargrove <gisellehargrove@gmail.com>
2019-07-26 12:47:44 -0400

    created episode card and list component

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -7,24 +7,27 @@ import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles({
   card: {
-    width: 300
+    width: 400
   }
 });
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
+export default function EpisodeCard({ name, airDate, episode, characters}) {
   const classes = useStyles();
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography gutterBottom variant="h3" component="h2">
+        <Typography gutterBottom variant="h4" component="h4">
           {name}
         </Typography>
-        <Typography gutterBottom variant="h4" component="h4">
-          {type} : {dimension}
+        <Typography gutterBottom variant="body1" component="p">
+          {episode} : {airDate}
+        </Typography>
+        <Typography gutterBottom variant="body1" component="p">
+          Number of Characters: {characters.length}
         </Typography>
       </CardContent>
     </Card>
-  )
-}
+
+  );
+};
--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import EpisodeCard from './EpisodeCard';
 
 export default function EpisodesList() {
+  const [episodes, setEpisodes] = useState({});
 
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/episode/').then(response => {
       console.log(response.data)
+      setEpisodes(response.data);
     });
   }, [])
+
+  if(!episodes.results) return <div>Loading...</div>
+
   return (
-    <div>Episodes List</div>
+    <section className='episodes-list grid-view'>
+      {episodes.results.map((episode) =>
+        <EpisodeCard
+          key={episode.id}
+          name={episode.name}
+          airDate={episode.air_date}
+          episode={episode.episode}
+          characters={episode.characters} />
+      )}
+    </section>
   )
 
 };

ab2023bc5ddaab9ccf587e189abe807af5a47266
gisellehargrove <gisellehargrove@gmail.com>
2019-07-26 12:27:20 -0400

    created location card

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles({
+  card: {
+    width: 300
+  }
+});
 
 export default function LocationCard ({ name, type, dimension, residents }) {
   // image={image}
-  return (<span>todo: location</span>
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography gutterBottom variant="h3" component="h2">
+          {name}
+        </Typography>
+        <Typography gutterBottom variant="h4" component="h4">
+          {type} : {dimension}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import LocationCard from './LocationCard';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    width: 400
+  }
+})
+
 
 export default function LocationsList() {
+  const classes = useStyles();
+  const [locations, setLocations] = useState({});
 
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/location/').then(response => {
+      console.log(response.data)
+      setLocations(response.data);
+    });
+  }, []);
+
+  if(!locations.results) return <div>Loading...</div>
+
+  return (<section className='locations-list grid-view'>
+
+    {locations.results.map((location) =>
+      <LocationCard
+        key={location.id}
+        name={location.name}
+        type={location.type}
+        dimension={location.dimension}
+        residents={location.residents.length} />
+    )}
+
+    </section>);
 }
